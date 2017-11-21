9ce3ba5cf777483c599f40956a3fd8bf2efd90bd
rebeckanylander <rebecka.nylander@hotmail.com>
2017-11-21 16:42:39 +0100

    added styles

--- code.js
+++ code.js
@@ -1,17 +1,34 @@
 function renderMovie(movie){
-	
-	var picture = document.getElementById("picture");
-	picture.src = movie.picUrl;
-	var movieTitle = document.getElementById("movieTitle");
-	movieTitle.innerText = movie.title;
-	var movieDescription = document.getElementById("movieDescription");
-	movieDescription.innerText = movie.description;
-	var actorsHeadline = document.getElementById("actorsHeadline");
-	actorsHeadline.innerText = movie.actorsHeadline;
-	var actorsList = document.getElementById("actorsList");
+	var picture = $("#picture");
+	picture.attr("src", movie.picUrl); // Bättre än picture.src = movie.picUrl
+	var movieTitle = $("#movieTitle");
+	movieTitle.text(movie.title);
+	var movieDescription = $("#movieDescription");
+	movieDescription.text(movie.description);
+	var actorsHeadline = $("#actorsHeadline");
+	actorsHeadline.text(movie.actorsHeadline);
+	var actorsList = $("#actorsList");
 	for(let i in movie.actors){
-		actorsList.innerHTML+="<li>"+movie.actors[i]+"</li>";
+		actorsList.append("<li>"+movie.actors[i]+"</li>");
 	}
+}
+
+for(let i=1; i<=5; i++){
+  let star = $("#star" + i);
+  star.click(function(){
+    changeStarRating(i);
+  });
+}
+
+function changeStarRating(rating){
+  for(let i=1; i<=5; i++){
+    let star = $("#star" + i);
+    if (i <= rating){
+      star.addClass("filled");
+    } else {
+      star.removeClass("filled");
+    }
+  }
 }
 
 renderMovie(movieData);
